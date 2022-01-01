76728f14c485ba81026eecbae801b84ad58dd0b2
Krishnarash <thevakumarankrishnarash@gmail.com>
2022-01-01 23:49:31 +0100

    Ajout du modal pour supprimer produit

---
+++ src/components/Modal/DeleteProductModal.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="bootstrap-modal-no-jquery">
+        <div class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content" style="background-color: #F0F4F8">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Supprimer le produit</h5>
+                    </div>
+                    <div class="modal-body">
+                        <p>Voulez-vous supprimer votre produit "{{info.label}}" ?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn bg-danger" @click="deleteProduct">Oui</button>
+                        <button type="button" class="btn bg-white" @click="closeModal">Annuler</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data: function () {
+        return {
+            info: '',
+        }
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close-modal-event');
+        },
+        deleteProduct() {
+            axios
+                .post('http://localhost:8080/product/delete/',{id: this.info.id} ,{useCredentails: true})
+                .then(res => {console.log(res.data);})
+                .catch(err => {console.log(err.response);
+                });
+            this.closeModal();
+        }
+    },
+    mounted() {
+        axios
+            .get('http://localhost:8080/product/galette')
+            .then(response => (this.info = response.data))
+            .catch(error => (console.log(error)))
+    }
+}
+</script>
+
+<style scoped>
+/* Override default value of 'none' */
+.modal {
+    display: block;
+}
+</style>

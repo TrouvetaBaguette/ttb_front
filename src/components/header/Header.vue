67b093320e7204e51c506b928ac3c7b45d257dba
Krishnarash <thevakumarankrishnarash@gmail.com>
2022-01-02 20:52:49 +0100

    Ajout du lien de la page commentaire dans l'header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -13,6 +13,9 @@
             <li>
                 <router-link to="/showProduct">Afficher Produit</router-link>
             </li>
+            <li>
+                <router-link to="/showComment">Commentaire</router-link>
+            </li>
         </ul>
         <button class="ms-auto btn px-4">Connexion</button>
     </nav>
